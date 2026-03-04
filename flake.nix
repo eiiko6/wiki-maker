ba6705d798f863ca3ef96aaa038090ba2ecdb191
eiiko6 <myeiiko6@gmail.com>
2026-03-04 17:26:10 +0100

    remvoved flake-utils which was not really useful

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
 
     code-theme = {
@@ -15,59 +14,56 @@
   outputs =
     {
       nixpkgs,
-      flake-utils,
       rust-overlay,
       code-theme,
       ...
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        overlays = [ rust-overlay.overlays.default ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-        rust = pkgs.rust-bin.stable.latest.default;
-        openssl = pkgs.openssl;
-      in
-      {
-        packages.default = pkgs.rustPlatform.buildRustPackage {
-          pname = "wiki-maker";
-          version = "0.1.0";
+    let
+      system = "x86_64-linux";
+      overlays = [ rust-overlay.overlays.default ];
+      pkgs = import nixpkgs {
+        inherit system overlays;
+      };
+      rust = pkgs.rust-bin.stable.latest.default;
+      openssl = pkgs.openssl;
+    in
+    {
+      packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
+        pname = "wiki-maker";
+        version = "0.1.0";
 
-          src = ./.;
+        src = ./.;
 
-          THEME_PATH = "${code-theme}";
+        THEME_PATH = "${code-theme}";
 
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-          };
-
-          nativeBuildInputs = [
-            rust
-            pkgs.pkg-config
-          ];
-
-          buildInputs = [
-            openssl
-          ];
-
-          OPENSSL_LIB_DIR = "${openssl.out}/lib";
-          OPENSSL_INCLUDE_DIR = "${openssl.dev}/include";
-          PKG_CONFIG_PATH = "${openssl.dev}/lib/pkgconfig";
+        cargoLock = {
+          lockFile = ./Cargo.lock;
         };
 
-        devShells.default = pkgs.mkShell {
-          packages = [
-            rust
-            pkgs.cargo
-            pkgs.rust-analyzer
-            pkgs.pkg-config
-            openssl
-          ];
-          OPENSSL_DIR = openssl.dev;
-          PKG_CONFIG_PATH = "${openssl.dev}/lib/pkgconfig";
-        };
-      }
-    );
+        nativeBuildInputs = [
+          rust
+          pkgs.pkg-config
+        ];
+
+        buildInputs = [
+          openssl
+        ];
+
+        OPENSSL_LIB_DIR = "${openssl.out}/lib";
+        OPENSSL_INCLUDE_DIR = "${openssl.dev}/include";
+        PKG_CONFIG_PATH = "${openssl.dev}/lib/pkgconfig";
+      };
+
+      devShells.default = pkgs.mkShell {
+        packages = [
+          rust
+          pkgs.cargo
+          pkgs.rust-analyzer
+          pkgs.pkg-config
+          openssl
+        ];
+        OPENSSL_DIR = openssl.dev;
+        PKG_CONFIG_PATH = "${openssl.dev}/lib/pkgconfig";
+      };
+    };
 }
